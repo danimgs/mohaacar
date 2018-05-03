f1be3c990df30d7753d918522112461a5779f177
danimgs <danimgs10@gmail.com>
2018-05-03 22:58:03 +0200

    Add files via upload
    lang

---
+++ lang/lang-EN.php
@@ -0,0 +1,298 @@
+<?php
+/*terceros*/
+$terceros="THIRD PARTY INSURANCE";
+$texto1="Any car insurance includes a series of minimum coverage. The most important is the 
+		liability of the driver. This coverage means that insurance will cover the damage, personal 
+		or material, caused to third persons by the policyholder. What is not going to cover are 
+		themselves harms the holder driver or their vehicle, suffering from although the occupants 
+		of your car that are covered, as are considered 'third'.";
+$texto2="The covers are fixed by law and are 15 million euros as maximum in case of damages and 
+		70 million in case of physical damage, regardless of how many are the victims of the accident.
+		Most third party insurance policies often include other coverages, such as roadside assistance, 
+		legal defense and insurance for own driver. Anyway, these coverages will vary depending on the 
+		company that contracted the insurance.";
+$aviso="* Notice: There is a certain type. Not all third party insurance cover the same thing. 
+		Or all-risk insurance have the same conditions. Much depends on what each company considers 
+		'essential' in defining a certain 'a third party', 'for all', etc.";
+/*tercerosAmpliado*/
+$tituloseguros="INSURANCE EXTENDED THIRD";
+$foto1="It is more affordable than a fully comprehensive insurance.
+  		It includes, in addition to everything that the company deems worthy
+   		to compose a third party insurance, in case of fire,
+   		theft and broken windows. These are the three most common cases,
+   		although as we have said, depending on the insurance company
+   		other coverage, such as the replacement vehicle will be included,
+   		legal expenses, etc.";
+$foto2="For car glass, this type of insurance cover breakage
+		windshield, rear window and side windows.
+		In some cases also enter the sunroof.
+		If there is a fire and the car is a total loss,
+		it is usual that during the first two years of life
+		the car is returned 100 per 100 of the amount.
+		If it is older, the venal or venal improved value will be paid.
+		It is the norm, but there are policies that pay the market value
+		car from day one. If the fire is partial and
+		there have been some damaged parts,
+		It will be paid for both these areas.";
+$texto3="And the last of the three cases that usually cover these
+  		 insurance is theft. We must differentiate between robbery and theft.
+  		 The first is considered to have been hard and the second no.
+  		 Some policies cover only two assumptions and other thefts.
+  		 It is also normal in these cases cover damage
+  		 in case of attempted theft.";
+$aviso="*Warning: There is no safe type.
+		 Not all third-party insurance covering the same.
+		 Not all risks insurance have the same conditions.
+		 They depend very much on what each company considers 'essential'
+		 when defining a safe as 'third parties', 'all risks', etc.";
+
+/*todoriesgo*/
+$tituloriesgo="ALL RISK INSURANCE";
+$parrafoa="Scope of benefits against all risks ";
+$a1="Compulsory Liability";
+$a2="Further liability";
+$a3="Claims for damages";
+$a4="Driver´s body injuries";
+$a5="Glass breakage";
+$a6="Travel assistance 24 hours";
+$a7="Advice traffic fines";
+$a8="Own damages";
+$a9="Extraordinary risk cover (Consortium).";
+$parrafob="Security Pack";
+$b1="Withdrawal of the driving license";
+$b2="Valid driver's license loss for loss of points";
+$b3="Liability for loading, transport and unloading";
+$b4="Liability for performance of passenger";
+$b5="liability at rest, including fire / explosion";
+$b6="Free choice of workshops to bring your car to your trusted workshop, or if you prefer";
+$b7="Repair our network of associated workshops.";
+$parrafoc="Club Lameros";
+$c1="¡At your disposal are more than 12,000 outlets!";
+$c2="Holidays, Travel, Entertainment, Restaurants";
+$c3="Home, Food";
+$c4="Fashion, Jewelry, Perfume";
+$c5="Health & Beauty";
+$c6="Motor and Automotive";
+$parrafod="Car insurance Lameros includes many amenities like:";
+$d1="Pick-up and repair Lameros,
+	 so you forget to take the car to the garage.
+ 	 AXA does it for you and offers you a courtesy car.";
+$d2="Site repair: for no fault spoil your trip.";
+$d3="Benefits for customers AXA, AXA Quality Workshops.";
+$d4="Web client, mobile applications such as AXA and AXA WithYou Drive ... and more.";
+$parrafoe="Quality guarantee";
+$e1="In Lameros we specialize in car insurance all risk.
+	 We have the most advanced technical means to
+	 provide services to our 1st level.";
+$e2="We offer you, service an extensive network of suppliers
+	 and highly skilled professionals to solve
+	 with maximum efficiency and speed any related management
+	 your insurance.";
+$e3="We are part of the Allianz Group insurance leader in Europe.";
+$e4="Our quality management system is certified by AENOR and IQNet";
+
+/*Todoriesgofranquicia*/
+$tituloriesgofranquicia="COMPREHENSIVE INSURANCE WITH FRANCHISE";
+$parrafoa1="Summary of coverages with excess";
+$a1a="Compulsory Liability";
+$a1b="Further liability";
+$a1c="Claims for damages";
+$a1d="Driver´s body injuries";
+$a1e="Travel assistance 24 hours and crane (Km 0)";
+$a1f="Advice traffic fines";
+$a1g="Thievery";
+$a1h="Fire, explosion and lightning";
+$a1i="Damage own franchise";
+$pararafob1="Choose the franchise you better come";
+$b1a="We find below several franchises
+	  your car insurance all risk,
+	  so you can choose the one that suits you.";
+$b1b="Always you know in advance the amount of the franchise.";
+$b1c="Calculate your budget and show you options for your profile.";
+$b1d="In car insurance All Risks Phoenix Good Driver,
+	  the franchise to own damages only applies to
+	  cases of impact and vandalism.";
+$b1e="Guarantees own damage by fire, explosion, theft,
+	  lightning strike and crystals, work without excess.";
+$b1f="Neither franchise applies in the case of that damage
+	  the insured vehicle is due to collision with another vehicle
+	  duly identified motor and driver is otherwise
+	  identified and found guilty.";
+$parrafoc1="Quality assurance for your insurance with excess";
+$c1a="A great professional team that offers personal attention and first class service.";
+$c1b="One of the most extensive networks of trusted workshops,
+	  with high standards of quality, exclusive benefits and
+ 	  preferential treatment to repair your car.";
+$c1c="Modern technology to assist you with maximum efficiency and speed.";
+$c1d="Quality of service guaranteed by our quality management system certified by AENOR and IQNet.";
+$c1e="We are part of the Allianz Group, the number one insurance group in Europe.";
+$parrafod1="Accidental Insurance benefits";
+$d1a="Complete and permanent disability due to accident";
+$d1b="Full disability from stroke, considered an accident at work";
+$d1c="Partial permanent disability resulting from an accident";
+$d1d="Accidental death";
+$d1e="Death from heart attack, considered as accident at work";
+/*cabecera*/
+$Registrate="Sign Up";
+$Acceder="Log In";
+$Nombre1="Dni: ";
+$Usuario="User id";
+$Password="Password: ";
+$Entrar="Enter";
+$invalida="ID card or password incorrect";
+$Vehiculo="Vehicles";
+$BMW="BMW";
+$FORD="FORD";
+$AUDI="AUDI";
+$FERRARI="FERRARI";
+$MERCEDES="MERCEDES";
+$Promociones="Promotions";
+$UltimasOfertas="Latest Offers";
+$KM0="KM 0";
+$Vehiculodeocasion="Used Vehicles";
+$Alquiler="Renting";
+$Seguros="Insurances";
+$Terceros="Third-Party";
+$Tercerosampliados="Third Party Extended";
+$Todoriesgo="All Risk";
+$Todoriesgoampliado="All Risk Extended"; 
+$Todoriesgoconfranquicia="All Risk with franchise";
+$Conocenos="Know Us";
+$Sedes="Headquarters";
+$Contacto="Contact";
+$Nuestroequipo="Our Team";
+$Historia="History";
+/*index*/
+$rey="Be the king of the road";
+$limites="Reach your limits";
+$presente="Present, past and future";
+$deshazte="Get rid of your fears";
+$conducir="¿Do you like drive?";
+$marca="Select your brand";
+/*registro*/
+$registroen="Log in Mohaacar";
+$datosdeacceso="Acces data";
+$Direcciondecorre="Email Addres*";
+$Contraseña1="Password*";
+$Repitacontraseña="Repeat Password*";
+$Datosdecontacto="Contact  Information";
+$Sexo="Sex*";
+$Hombre="Man";
+$Mujer="Woman";
+$Otros="Others";
+$Nombre="Name*";
+$PrimerApellido="Surname*";
+$SegundoApellido="Second surname*";
+$DNI="DNI*";
+$Telefono="Telephone Number";
+$NotaLegal="Legal note";
+$comentario="Comment";
+$contacta="CONTACT US";
+$Textaco1="You authorize that your personal data will be incorporated into
+ 			a file which is responsible MOHAACAR, for the proper administration of the
+ 			relations between the parties, conduct opinion polls and send information
+  			including by electronic means from the segmentation of the information in 
+ 			file, product or marketed by MOHAACAR, BMW Group companies (currently 
+  		 	MOHAACAR, whose business is the manufacture, sale, leasing, renting and
+ 		  	banking and financial services respectively) or by another company 
+			related to the car and motorcycle services, financial and insurance sector 
+			that may be of interest. It also authorizes MOHAACAR to cede these data, 
+			for the same purpose, the companies said BMW Group and its network of 
+			authorized dealers and workshops and any other entity in the automotive,
+	 		motorcycle, financial or insurance sector, considered informed of the
+	  		assignment. In accordance with Law 15/1999, you. You can exercise your
+	   		right to opposition, access, correction or deletion of data by 
+	   		application accompanied by a photocopy of your identity document,
+	    	addressed to MOHAACAR, CRM Department, Avda. De Burgos, 118, 28050
+	     	Madrid.";
+$Heleido="I have read and accept the legal notice";
+$enviar="Submit";
+
+//Datos usuario
+$MisDatos="MY DATA";
+$datospersonales="PERSONAL DATA";
+$vehiculoscomprados="PURCHASED CARS";
+$alquilerdecoches="RENTING CARS";
+$modificaciones="DATA MODIFICATION";
+$nombre="NAME";
+$apellidos="SURNAMES";
+$dni="DNI";
+$correo="MAIL";
+$telefono="PHONE";
+$Contraseña="PASSWORD";
+$modificar="Data modification";
+
+//DATOS USUARIOS2
+
+/*$MisDatos="MIS DATOS";
+$datospersonales="DATOS PERSONALES";
+$vehiculoscomprados="VEHICULOS COMPRADOS";
+$alquilerdecoches="ALQUILER DE COCHES";
+$modificaciones="MODIFICACION DE DATOS";
+*/
+$matricula="REGISTRATION";
+$marca="BRAND";
+$fechadecompra="DATE OF PURCHASE";
+
+
+//DATOS USUARIOS3
+
+/*$MisDatos="MIS DATOS";
+$datospersonales="DATOS PERSONALES";
+$vehiculoscomprados="VEHICULOS COMPRADOS";
+$alquilerdecoches="ALQUILER DE COCHES";
+$modificaciones="MODIFICACION DE DATOS";
+*/
+$matricula="REGISTRATION";
+$fechaderecogida="PICK UP DATE";
+$fechadedevolucion="RETURN DATE";
+$marca="BRAND";
+$zonadealquiler="CHARTER AREA";
+$nuevoalquiler="Cancel rental";
+
+//DATOS USUARIOS4
+
+/*$MisDatos="MIS DATOS";
+$datospersonales="DATOS PERSONALES";
+$vehiculoscomprados="VEHICULOS COMPRADOS";
+$alquilerdecoches="ALQUILER DE COCHES";
+$modificaciones="MODIFICACION DE DATOS";
+*/
+$nombre="NAME";
+$apellidos="SURNAMES";
+$dni="DNI";
+$correo="MAIL";
+$telefono="PHONE";
+$Contraseña="PASSWORD";
+$modificar="Data modification";
+$borrado="Delete account";
+
+//CUENTA
+$Bienvenido="Welcome";
+$Desconexion="disconnection";
+$Area="client area";
+
+
+//ALQUILER
+$haz="¡MAKE YOUR RESERVATION!";
+$recogida="Pick up date";
+$devolucion="Date Due";
+$errorCampo="Please complete the fields";
+$errorFecha="Enter the dates correctly";
+$marcaAlquiler="Brand";
+$modeloAlquiler="Model";
+$matriculaAlquiler="Enrollment";
+$lugar="Place of pickup and dropoff";
+$seleccione="--Select Brand--";
+$seleccione1="--Select a model--";
+$seleccione2="--Select an enrollment--";
+$reservar="Reserve";
+$cancelar="Cancel booking";
+$informacion="TO BE ABLE TO REALIZE A RENT IT MUST INITIATE MEETING";
+$informacion1="OR IN HIS FAULT IT CAN";
+$aqui="TO REGISTER HERE";
+//IMAGENES
+$imagenesPromo= array("ANNINGLES.jpg","bmwIngles.jpg","COMPETISin.jpeg","fiestaIngles.jpg","huracanIngles.jpg");
+$conozca="MEET OUR FACILITIES";
+$nuestroEquipo="OUR TEAM OF PROFESSIONALS";
+?>
---
+++ lang/lang-ES.php
@@ -0,0 +1,306 @@
+<?php
+/*terceros*/
+$terceros="SEGURO A TERCEROS";
+$texto1="Cualquier seguro de coche incluye una serie de coberturas mínimas. 
+		La más importante es la responsabilidad civil del conductor. 
+		Esta cobertura implica que el seguro cubrirá los daños, personales o materiales, 
+		causados a terceras personas por el titular del seguro. Lo que no va a cubrir son 
+		los daños propios que sufran el conductor titular o su vehículo, aunque los demás 
+		ocupantes de su coche sí que están cubiertos, ya que se consideran 'terceros'.";
+$texto2="Las coberturas están fijadas por ley y son 15 millones de euros como máximo en caso
+		de daños materiales y 70 millones en caso de daños físicos, independientemente de cuántas sean
+		las víctimas del accidente. La mayoría de las pólizas de seguros a terceros suelen incluir 
+		otras coberturas, como la asistencia en carretera, la defensa jurídica o un seguro para el 
+		propio conductor. De todas formas, estas coberturas variarán dependiendo de la compañía con 
+		la que se contrate el seguro.";
+$aviso="*Aviso: No hay un seguro tipo .No todos los seguros a terceros cubren lo mismo. 
+		Ni los seguros a todo riesgo tienen las mismas condiciones. Dependen mucho de lo que cada 
+		compañía considere “imprescindible” a la hora de definir un seguro como “a terceros”, “a todo 
+		riesgo”, etc.";
+/*tercerosAmpliado*/
+
+$tituloseguros="SEGURO A TERCEROS AMPLIADO";
+$foto1="Es un seguro más asequible que uno a todo riesgo.
+ 		Incluye, además de todo lo que la compañía considere digno
+  		de componer un seguro a terceros, los casos de incendio,
+  		robo y rotura de lunas. Estos son los tres casos más habituales,
+  		aunque como ya hemos dicho, dependiendo de la compañía aseguradora
+  		se incluirán otras coberturas, como el vehículo de sustitución,
+  		defensa jurídica, etc.";
+$foto2="En el caso de las lunas, este tipo de seguros cubrirá la rotura
+		del parabrisas, la luneta trasera y las ventanillas laterales.
+		En algunos casos entrará también el techo solar.
+		Si hay un incendio y el coche queda siniestro total,
+		lo habitual es que durante los dos primeros años de vida
+		del coche se devuelva el 100 por 100 del importe.
+		Si es más viejo, se pagará el valor venal o venal mejorado.
+		Es lo habitual, pero hay pólizas que pagan el valor venal
+		del coche desde el primer día. Si el incendio es parcial y
+		ha habido algunas partes dañadas,
+		se pagará por esas partes concretas.";
+$texto3="Y el último de los tres casos que suelen cubrir estos
+ 		 seguros es el de robo. Hay que diferenciar entre robo y hurto.
+ 		 En el primero se considera que ha habido fuerza y en el segundo no.
+ 		 Algunas pólizas cubren ambos supuestos y otras solo los robos.
+ 		 También es normal cubrir en estos casos los daños ocasionados
+ 		 en caso de intento de robo.";
+$aviso="*Aviso: No hay un seguro tipo. 
+		No todos los seguros a terceros cubren lo mismo.
+		Ni los seguros a todo riesgo tienen las mismas condiciones.
+		Dependen mucho de lo que cada compañía considere “imprescindible”
+		a la hora de definir un seguro como “a terceros”, “a todo riesgo”, etc.";
+
+/*todoriesgo*/
+$tituloriesgo="SEGURO A TODO RIESGO";
+$parrafoa="Resumen de coberturas a todo riesgo ";
+$a1="Responsabilidad civil obligatoria";
+$a2="Responsabilidad civil complementaria";
+$a3="Reclamación de daños";
+$a4="Accidentes corporales del conductor";
+$a5="Rotura de cristales";
+$a6="Asistencia en viaje 24 horas";
+$a7="Asesoramiento por multas de tráfico";
+$a8="Daños Propios";
+$a9="Cobertura de riesgos extraordinarios (Consorcio).";
+$parrafob="Pack de seguridad";
+$b1="Retirada del permiso de conducir";
+$b2="Pérdida vigencia permiso de conducir por pérdida de puntos";
+$b3="Responsabilidad civil por carga, transporte y descarga";
+$b4="Responsabilidad civil por actuación de pasajeros";
+$b5="Responsabilidad civil en reposo, incluyo por incendio/explosión";
+$b6="Libre Elección de talleres para llevar tu coche a tu taller de confianza, o si prefieres";
+$b7="Reparación en nuestra red de Talleres Concertados.";
+$parrafoc="Club Lameros";
+$c1="¡Tienes a tu disposición más de 12.000 puntos de venta!";
+$c2="Vacaciones, Viajes, Ocio, Restaurantes";
+$c3="Hogar, Alimentación";
+$c4="Moda, Joyería, Perfumería";
+$c5="Salud y belleza";
+$c6="Motor y Automoción";
+$parrafod="El seguro de coche Lameros incluye numerosos servicios, como:";
+$d1="Servicio de recogida y reparación Lameros,
+	 para que te olvides de llevar el coche al taller.
+	 AXA lo hace por ti y te ofrece un coche de cortesía.";
+$d2="Reparación in situ: para que ninguna avería estropee tu viaje.";
+$d3="Ventajas exclusivas para clientes AXA en los Talleres AXA Calidad.";
+$d4="Web cliente, aplicaciones para móviles como AXA Contigo y AXA Drive...y mucho más.";
+$parrafoe="Garantia de calidad";
+$e1="En Lameros somos especialistas en seguros de coche a todo riesgo.
+	 Contamos con los medios técnicos más avanzados para 
+	 ofrecer a nuestros asegurados servicios de 1er nivel.";
+$e2="Ponemos a tu servicio una extensa red de proveedores
+	 y profesionales altamente cualificados para resolver
+	 con la máxima eficacia y celeridad cualquier gestión relacionada
+	 con tu seguro.";
+$e3="Formamos parte del Grupo Allianz lider asegurador en Europa.";
+$e4="Nuestro Sistema de Gestión de la Calidad está Certificado por AENOR e IQNet";
+
+
+/*Todoriesgofranquicia*/
+$tituloriesgofranquicia="SEGURO A TODO RIESGO CON FRANQUICIA";
+$parrafoa1="Resumen de coberturas a todo riesgo con franquicia";
+$a1a="Responsabilidad civil obligatoria";
+$a1b="Responsabilidad civil complementaria";
+$a1c="Reclamación de daños";
+$a1d="Accidentes corporales del conductor";
+$a1e="Asistencia en viaje 24 horas y grúa (Km.0)";
+$a1f="Asesoramiento por multas de tráfico";
+$a1g="Robo";
+$a1h="Incendio, explosión y rayo";
+$a1i="Daños propios con franquicia";
+$pararafob1="Escoge la franquicia que te venga mejor";
+$b1a="Ponemos a tu disposición varias franquicias para
+	  tu seguro de coche a todo riesgo,
+	  para que elijas la que más te convenga.";
+$b1b="Siempre sabrás con antelación el importe de la franquicia.";
+$b1c="Calcula tu presupuesto y te mostramos las opciones para tu perfil.";
+$b1d="En el seguro de coche Todo Riesgo de Fénix Buen Conductor,
+	  la franquicia para daños propios únicamente se aplica para
+	  los casos de impacto y vandalismo.";
+$b1e="Las garantías de daños propios por incendio, explosión, robo,
+	  caída de rayo y cristales, funcionan sin franquicia.";
+$b1f="Tampoco se aplica la franquicia en el caso de que los daños
+	  del vehículo asegurado se deban a colisión con otro vehículo
+	  a motor debidamente identificado y el conductor contrario esté
+	  identificado y resulte culpable.";
+$parrafoc1="Garantia de calidad en tu seguro Todo Riesgo con Franquicia";
+$c1a="Un gran equipo profesional que te ofrece atención personal y servicio de primera clase.";
+$c1b="Una de las redes más amplias de talleres de confianza,
+	  con altos estándares de calidad, ventajas exclusivas y
+	  trato preferente en la reparación de tu coche.";
+$c1c="Modernos medios técnicos para atenderte con la máxima eficacia y rapidez.";
+$c1d="Calidad de servicio avalada por nuestro Sistema de Gestión de la Calidad Certificado por AENOR e IQNet.";
+$c1e="Somos parte del grupo Allianz, el grupo asegurador número 1 de Europa.";
+$parrafod1="Prestaciones Seguros Accidentales";
+$d1a="Invalidez completa y permanente por accidente";
+$d1b="Invalidez completa por infarto, considerado accidente de trabajo";
+$d1c="Invalidez permanente parcial a consecuencia de accidente";
+$d1d="Fallecimiento por accidente";
+$d1e="Fallecimiento por infarto, considerado accidente de trabajo";
+
+/*cabecera*/
+$Registrate="Registrate";
+$Acceder="Acceder";
+$Nombre1="Dni: ";
+$Usuario="Dni de usuario";
+$Password="Contraseña: ";
+$Entrar="Entrar";
+$invalida="Dni o contraseña incorrectos";
+$Vehiculo="Vehiculo";
+$Promociones="Promociones";
+$UltimasOfertas="Ultimas Ofertas";
+$KM0="KM 0";
+$Vehiculodeocasion="Vehiculo de ocasion";
+$Alquiler="Alquiler";
+$Seguros="Seguros";
+$Terceros="Terceros";
+$Tercerosampliados="Terceros ampliados";
+$Todoriesgo="Todo riesgo";
+$Todoriesgoampliado="Todo riesgo ampliado"; 
+$Todoriesgoconfranquicia="Todo riesgo con franquicia";
+$Conocenos="Conócenos";
+$Sedes="Sedes";
+$Contacto="Contacto";
+$Nuestroequipo="Nuestro equipo";
+$Historia="Historia";
+/*index*/
+$rey="Se el rey de la carretera";
+$limites="Alcanza tus limites";
+$presente="Presente, pasado y futuro";
+$deshazte="Deshazte de tus miedos";
+$conducir="¿Te gusta conducir?";
+$marca="Selecciona tu marca";
+/*registro*/
+$registroen="Registro en Mohaacar";
+$datosdeacceso="Datos de acceso";
+$Direcciondecorre="Direccion de correo electronico*";
+$Contraseña1="Contraseña*";
+$Repitacontraseña="Repetir Contraseña*";
+$Datosdecontacto="Datos de contacto";
+$Sexo="Sexo*";
+$Hombre="Hombre";
+$Mujer="Mujer";
+$Otros="Otros";
+$Nombre="Nombre*";
+$PrimerApellido="Primer Apellido*";
+$SegundoApellido="Segundo Apellido*";
+$DNI="DNI*";
+$Telefono="Teléfono*";
+$NotaLegal="Nota Legal";
+$comentario="Comentario";
+$contacta="CONTACTA CON NOSOTROS";
+$Textaco1="El interesado autoriza que sus datos personales sean incorporados
+		   a un fichero del que es responsable MOHAACAR, para la 
+		   correcta administración de las relaciones entre las partes, 
+		   realizar encuestas de opinión y remitirle información incluso 
+		   por medios electrónicos a partir de la segmentación de la información
+		   existente en el fichero, sobre productos o servicios comercializados
+		   por MOHAACAR, empresas del Grupo BMW (Actualmente MOHAACAR, cuya
+		   actividad es la fabricación, venta, leasing, renting y 
+		   servicios bancarios y financieros respectivamente) o por terceras
+		   empresas relacionadas con el automóvil y la motocicleta, el 
+		   sector financiero y asegurador que puedan ser de su interés.
+		   Asimismo, autoriza a MOHAACAR para que ceda estos 
+		   datos, con la misma finalidad, a las empresas del Grupo BMW 
+		   citadas, así como a su red de Concesionarios y Talleres Autorizados
+		   y a cualquier otra entidad del sector de la automoción,
+		   motociclismo, financiero o asegurador, considerándose informado
+		   de dicha cesión. De conformidad con la Ley Orgánica 15/1999, 
+		   Vd. podrá ejercer los derechos de oposición, acceso, rectificación 
+		   o cancelación de sus datos mediante solicitud acompañada de fotocopia
+		   de su documento de identidad, dirigida a MOHAACAR, Departamento 
+		   CRM, Avda. de Burgos, 118, 28050 Madrid.";
+$Heleido="He leido y acepto la nota legal";
+$enviar="Enviar";
+
+//Datos usuario
+$MisDatos="MIS DATOS";
+$datospersonales="DATOS PERSONALES";
+$vehiculoscomprados="VEHICULOS COMPRADOS";
+$alquilerdecoches="ALQUILER DE COCHES";
+$modificaciones="MODIFICACION DE DATOS";
+$nombre="NOMBRE";
+$apellidos="APELLIDOS";
+$dni="DNI";
+$correo="CORREO";
+$telefono="TELEFONO";
+$Contraseña="CONTRASEÑA";
+$modificar="Modificar Datos";
+
+//DATOS USUARIOS2
+
+/*$MisDatos="MIS DATOS";
+$datospersonales="DATOS PERSONALES";
+$vehiculoscomprados="VEHICULOS COMPRADOS";
+$alquilerdecoches="ALQUILER DE COCHES";
+$modificaciones="MODIFICACION DE DATOS";
+*/
+$matricula="MATRICULA";
+$marca="MARCA";
+$fechadecompra="FECHA DE COMPRA";
+
+
+//DATOS USUARIOS3
+
+/*$MisDatos="MIS DATOS";
+$datospersonales="DATOS PERSONALES";
+$vehiculoscomprados="VEHICULOS COMPRADOS";
+$alquilerdecoches="ALQUILER DE COCHES";
+$modificaciones="MODIFICACION DE DATOS";
+*/
+$matricula="MATRICULA";
+$fechaderecogida="FECHA DE RECOGIDA";
+$fechadedevolucion="FECHA DE DEVOLUCION";
+$marca="MARCA";
+$zonadealquiler="ZONA DE ALQUILER";
+$nuevoalquiler="Cancelar alquiler";
+
+//DATOS USUARIOS4
+
+/*$MisDatos="MIS DATOS";
+$datospersonales="DATOS PERSONALES";
+$vehiculoscomprados="VEHICULOS COMPRADOS";
+$alquilerdecoches="ALQUILER DE COCHES";
+$modificaciones="MODIFICACION DE DATOS";
+*/
+$nombre="NOMBRE";
+$apellidos="APELLIDOS";
+$dni="DNI";
+$correo="CORREO";
+$telefono="TELEFONO";
+$Contraseña="CONTRASEÑA";
+$modificar="Modificar Datos";
+$borrado="Eliminar Cuenta";
+
+
+//CUENTA
+$Bienvenido="Bienvenido";
+$Desconexion="Desconexion";
+$Area="Area cliente";
+
+//ALQUILER
+$haz="¡HAZ TU RESERVA!";
+$recogida="Fecha de recogida";
+$devolucion="Fecha de devolucion";
+$errorCampo="Por favor completa los campos";
+$errorFecha="Introduzca las fechas correctamente";
+$marcaAlquiler="Marca";
+$modeloAlquiler="Modelo";
+$matriculaAlquiler="Matricula";
+$lugar="Lugar de recogida y devolucion";
+$seleccione="--Seleccione una marca--";
+$seleccione1="--Seleccione un modelo--";
+$seleccione2="--Seleccione una matricula--";
+$reservar="Reservar";
+$cancelar="Cancelar reserva";
+$informacion="PARA PODER REALIZAR UN ALQUILER DEBE INICIAR SESION ";
+$informacion1="O EN SU DEFECTO PUEDE";
+$aqui="REGISTRARSE AQUI";
+
+//IMAGENES
+$imagenesPromo= array("ANNESPAÑOL.jpg","bmwEspañol.jpg","COMPETISes.jpeg","fiestaEspañol.jpg","huracanEspañol.jpg");
+
+//SEDES
+$conozca="CONOZCA NUESTRAS INSTALACIONES";
+$nuestroEquipo="NUESTRO EQUIPO DE PROFESIONALES";
+?>
